2ebe555cbbbde098e24c1ea38f4829af682cdc03
Jswqzx <jswqzx@qq.com>
2024-07-02 14:26:33 +0800

    process

--- Lession1/first.cpp
+++ Lession1/first.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main(void)
 {
-    cout << "Hello World" << endl;
+    // son process
+    // int flag = system("ping www.baidu.com");
+    // if(flag != 0)
+    // {
+    //     cout << "Son runing";
+    // }
+
+    //cout << getpid() << endl;
+
+    pid_t pid = fork();
+
+    cout << "current process Pid: " << pid << endl;
+
+    cout << "I've got you in my sight " << endl;
+    
     return 0;
 }
